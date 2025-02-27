1fbf43862ff6b39dedfea28f1a3bc34f9b275fc2
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2025-02-27 16:47:33 +0700

    feat: review

--- lib/constants/image_constant.dart
+++ lib/constants/image_constant.dart
@@ -1,4 +1,5 @@
-class ImageConstant {
+class 
+ImageConstant {
   static const String logo = 'assets/images/ic_javacode.png';
   static const String google = 'assets/images/ic_google.png';
   static const String map = 'assets/images/bg_map.png';
--- lib/features/profile/sub_features/rating/view/ui/add_new_rating.dart
+++ lib/features/profile/sub_features/rating/view/ui/add_new_rating.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
+import 'package:venturo_core/configs/themes/main_color.dart';
 import 'package:venturo_core/features/profile/sub_features/rating/controllers/rating_controller.dart';
 import 'package:venturo_core/shared/models/rating.dart';
 
 class AddNewRating extends StatelessWidget {
   final RatingController ratingController = Get.put(RatingController());
+
+  final Map<int, String> ratingTexts = {
+    1: 'Sangat Buruk',
+    2: 'Buruk',
+    3: 'Cukup',
+    4: 'Baik',
+    5: 'Sempurna'
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -13,79 +23,159 @@ class AddNewRating extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Penilaian'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-             const Text(
-              'Berikan Penilaianmu',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Obx(() => RatingBar.builder(
-                  initialRating: ratingController.rating.value,
-                  minRating: 1,
-                  direction: Axis.horizontal,
-                  allowHalfRating: true,
-                  itemCount: 5,
-                  itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                  itemBuilder: (context, _) => const Icon(
-                    Icons.star,
-                    color: Colors.amber,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Berikan Penilaianmu',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Row(
+                          children: [
+                            Obx(() => RatingBar.builder(
+                                  initialRating: ratingController.rating.value,
+                                  minRating: 1,
+                                  direction: Axis.horizontal,
+                                  allowHalfRating: true,
+                                  itemCount: 5,
+                                  itemPadding: const EdgeInsets.symmetric(
+                                      horizontal: 4.0),
+                                  itemBuilder: (context, _) => const Icon(
+                                    Icons.star,
+                                    color: Colors.amber,
+                                  ),
+                                  onRatingUpdate: (rating) {
+                                    ratingController.updateRating(rating);
+                                  },
+                                )),
+                            const Spacer(),
+                            Obx(() => Text(
+                                  ratingTexts[ratingController.rating.value
+                                          .round()] ??
+                                      '',
+                                  style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                )),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                  onRatingUpdate: (rating) {
-                    ratingController.updateRating(rating);
-                  },
-                )),
-            const SizedBox(height: 16),
-            const Text(
-              'Apa yang perlu ditingkatkan?',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Obx(() => Wrap(
-                  spacing: 8.0,
-                  children: ratingController.categories.map((category) {
-                    return ChoiceChip(
-                      label: Text(category),
-                      selected:
-                          ratingController.selectedCategory.value == category,
-                      onSelected: (selected) {
-                        ratingController.updateCategory(category);
-                      },
-                    );
-                  }).toList(),
-                )),
-            const SizedBox(height: 16),
-           
-            const Text(
-              'Tulis Review',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            TextField(
-              onChanged: (value) {
-                ratingController.updateDescription(value);
-              },
-              maxLines: 4,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Tulis Review Anda',
+                  const SizedBox(height: 16),
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Apa yang perlu ditingkatkan?',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Obx(() => Wrap(
+                              spacing: 8.0,
+                              children:
+                                  ratingController.categories.map((category) {
+                                return ChoiceChip(
+                                  disabledColor: MainColor.grey,
+                                  backgroundColor: Colors.grey[200],
+                                  selectedColor: MainColor.white,
+                                  side: const BorderSide(
+                                      color: MainColor.primary),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(60),
+                                  ),
+                                  label: Text(
+                                    category,
+                                    style: const TextStyle(
+                                        color: MainColor.primary),
+                                  ),
+                                  selected:
+                                      ratingController.selectedCategory.value ==
+                                          category,
+                                  onSelected: (selected) {
+                                    ratingController.updateCategory(category);
+                                  },
+                                );
+                              }).toList(),
+                            )),
+                        const SizedBox(height: 16),
+                        const Divider(),
+                        const Text(
+                          'Tulis Review',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        TextField(
+                          onChanged: (value) {
+                            ratingController.updateDescription(value);
+                          },
+                          maxLines: 4,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30)),
+                            ),
+                            hintText: 'Tulis Review Anda',
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: MainColor.primary,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(60),
+                              ),
+                            ),
+                            onPressed: () {
+                              final newRating = RatingModel(
+                                category:
+                                    ratingController.selectedCategory.value,
+                                rating: ratingController.rating.value,
+                                description: ratingController.description.value,
+                              );
+
+                              Get.back(
+                                  result:
+                                      newRating); // Return the new rating to the previous screen
+                            },
+                            child: const Text(
+                              'Kirim Penilaian',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                final newRating = RatingModel(
-                  category: ratingController.selectedCategory.value,
-                  rating: ratingController.rating.value,
-                  description: ratingController.description.value,
-                );
-
-                Get.back(
-                    result: newRating); // Pastikan mengembalikan RatingModel
-              },
-              child: const Text('Submit Rating'),
-            ),
-          ],
+          ),
         ),
       ),
     );
