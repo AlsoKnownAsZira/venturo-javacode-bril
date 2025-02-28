2ffe744c879a5e5a998097f6dd422052dcb18a9b
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2025-02-28 14:53:31 +0700

    fix: global binding

--- lib/shared/bindings/global_binding.dart
+++ lib/shared/bindings/global_binding.dart
@@ -1,9 +1,10 @@
 import 'package:get/get.dart';
 import 'package:venturo_core/shared/controllers/global_controllers/initial_controller.dart';
-
+import 'package:venturo_core/features/sign_in/controllers/sign_in_controller.dart';
 class GlobalBinding implements Bindings {
- @override
- void dependencies() {
-   Get.put(GlobalController());
- }
+  @override
+  void dependencies() {
+    Get.put(GlobalController());
+    Get.put(SignInController());
+  }
 }
